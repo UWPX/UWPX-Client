5fb8f89a0d10a22f4173842057c8fa47fdda02be
COM8 <sauter.fabian@mailbox.org>
2018-03-05 14:37:57 +0100

    Code cleanup

--- XMPP_API/Classes/Network/XMPPConnection.cs
+++ XMPP_API/Classes/Network/XMPPConnection.cs
@@ -1,7 +1,6 @@
 ﻿using Logging;
 using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using Thread_Save_Components.Classes.Collections;
 using Windows.System.Threading;
