7a0792ee59e2aeb2c1840057ae6f85258bde9f6d
COM8 <sauter.fabian@mailbox.org>
2018-03-26 20:19:06 +0200

    Removed unused import

--- XMPP_API/Classes/Network/TCP/TCPConnection.cs
+++ XMPP_API/Classes/Network/TCP/TCPConnection.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Windows.Foundation;
 using Windows.Networking;
 using Windows.Networking.Sockets;
 using Windows.Security.Cryptography.Certificates;
