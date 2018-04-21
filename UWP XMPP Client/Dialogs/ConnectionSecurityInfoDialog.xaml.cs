8bffea564f1ed3d42edfeb34e9bf60f3017d013d
COM8 <sauter.fabian@mailbox.org>
2018-04-21 19:22:13 +0200

    Started adding a certificate info dialog

---
+++ UWP XMPP Client/Dialogs/ConnectionSecurityInfoDialog.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Windows.Security.Cryptography.Certificates;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UWP_XMPP_Client.Dialogs
+{
+    public sealed partial class ConnectionSecurityInfoDialog : ContentDialog
+    {
+        //--------------------------------------------------------Attributes:-----------------------------------------------------------------\\
+        #region --Attributes--
+        public Certificate Cert
+        {
+            get { return (Certificate)GetValue(CertProperty); }
+            set { SetValue(CertProperty, value); }
+        }
+        public static readonly DependencyProperty CertProperty = DependencyProperty.Register("Cert", typeof(Certificate), typeof(ConnectionSecurityInfoDialog), null);
+
+        #endregion
+        //--------------------------------------------------------Constructor:----------------------------------------------------------------\\
+        #region --Constructors--
+        /// <summary>
+        /// Basic Constructor
+        /// </summary>
+        /// <history>
+        /// 17/03/2018 Created [Fabian Sauter]
+        /// </history>
+        public ConnectionSecurityInfoDialog()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+        //--------------------------------------------------------Set-, Get- Methods:---------------------------------------------------------\\
+        #region --Set-, Get- Methods--
+
+
+        #endregion
+        //--------------------------------------------------------Misc Methods:---------------------------------------------------------------\\
+        #region --Misc Methods (Public)--
+
+
+        #endregion
+
+        #region --Misc Methods (Private)--
+
+
+        #endregion
+
+        #region --Misc Methods (Protected)--
+
+
+        #endregion
+        //--------------------------------------------------------Events:---------------------------------------------------------------------\\
+        #region --Events--
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+
+        #endregion
+    }
+}
