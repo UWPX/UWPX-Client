1cd8367e9bf73f1918475aacdb72cb7fef359112
COM8 <sauter.fabian@mailbox.org>
2018-06-27 20:03:32 +0200

    First pass for the bookmarks master control

---
+++ UWP XMPP Client/DataTemplates/BoolVisabilityValueConverter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace UWP_XMPP_Client.DataTemplates
+{
+    class BoolVisabilityValueConverter : IValueConverter
+    {
+        //--------------------------------------------------------Attributes:-----------------------------------------------------------------\\
+        #region --Attributes--
+
+
+        #endregion
+        //--------------------------------------------------------Constructor:----------------------------------------------------------------\\
+        #region --Constructors--
+        /// <summary>
+        /// Basic Constructor
+        /// </summary>
+        /// <history>
+        /// 27/06/2018 Created [Fabian Sauter]
+        /// </history>
+        public BoolVisabilityValueConverter()
+        {
+        }
+
+        #endregion
+        //--------------------------------------------------------Set-, Get- Methods:---------------------------------------------------------\\
+        #region --Set-, Get- Methods--
+
+
+        #endregion
+        //--------------------------------------------------------Misc Methods:---------------------------------------------------------------\\
+        #region --Misc Methods (Public)--
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool b)
+            {
+                return b ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Visibility v)
+            {
+                return v == Visibility.Visible;
+            }
+            return false;
+        }
+
+        #endregion
+
+        #region --Misc Methods (Private)--
+
+
+        #endregion
+
+        #region --Misc Methods (Protected)--
+
+
+        #endregion
+        //--------------------------------------------------------Events:---------------------------------------------------------------------\\
+        #region --Events--
+
+
+        #endregion
+    }
+}
---
+++ UWP XMPP Client/DataTemplates/StringEmptyVisabilityValueConverter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace UWP_XMPP_Client.DataTemplates
+{
+    class StringEmptyVisabilityValueConverter : IValueConverter
+    {
+        //--------------------------------------------------------Attributes:-----------------------------------------------------------------\\
+        #region --Attributes--
+
+
+        #endregion
+        //--------------------------------------------------------Constructor:----------------------------------------------------------------\\
+        #region --Constructors--
+        /// <summary>
+        /// Basic Constructor
+        /// </summary>
+        /// <history>
+        /// 27/06/2018 Created [Fabian Sauter]
+        /// </history>
+        public StringEmptyVisabilityValueConverter()
+        {
+        }
+
+        #endregion
+        //--------------------------------------------------------Set-, Get- Methods:---------------------------------------------------------\\
+        #region --Set-, Get- Methods--
+
+
+        #endregion
+        //--------------------------------------------------------Misc Methods:---------------------------------------------------------------\\
+        #region --Misc Methods (Public)--
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is string s)
+            {
+                return string.IsNullOrEmpty(s) ? Visibility.Collapsed : Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region --Misc Methods (Private)--
+
+
+        #endregion
+
+        #region --Misc Methods (Protected)--
+
+
+        #endregion
+        //--------------------------------------------------------Events:---------------------------------------------------------------------\\
+        #region --Events--
+
+
+        #endregion
+    }
+}
