e57602c3a5747ce2761fc9e39feb6f4800c60b9d
COM8 <sauter.fabian@mailbox.org>
2018-08-09 10:26:42 +0200

    UI improvements

--- UWP XMPP Client/Dialogs/InitialStartDialog.xaml.cs
+++ UWP XMPP Client/Dialogs/InitialStartDialog.xaml.cs
@@ -48,11 +48,6 @@ namespace UWP_XMPP_Client.Dialogs
         #endregion
         //--------------------------------------------------------Events:---------------------------------------------------------------------\\
         #region --Events--
-        private async void viewOnGithub_tblck_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
-        {
-            await UiUtils.launchUriAsync(new System.Uri("https://github.com/UWPX/UWPX-Client"));
-        }
-
         private void close_btn_Click(object sender, RoutedEventArgs e)
         {
             showOnStartup = (bool)showOnStartup_cbx.IsChecked;
