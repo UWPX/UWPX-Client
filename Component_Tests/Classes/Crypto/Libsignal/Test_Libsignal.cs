19cf23d0e76c8bc7480f2163c8cfe7c8c8560979
COM8 <sauter.fabian@mailbox.org>
2018-11-11 12:36:03 +0100

    Added a libsignal public key deserialization test

--- Component_Tests/Classes/Crypto/Libsignal/Test_Libsignal.cs
+++ Component_Tests/Classes/Crypto/Libsignal/Test_Libsignal.cs
@@ -1,8 +1,10 @@
 ﻿using libsignal;
+using libsignal.ecc;
 using libsignal.protocol;
 using libsignal.state;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using XMPP_API.Classes.Crypto;
 
@@ -11,8 +13,8 @@ namespace Component_Tests.Classes.Crypto.Libsignal
     [TestClass]
     public class Test_Libsignal
     {
-        SignalProtocolAddress aliceAddress = new SignalProtocolAddress("alice@example.com", 1);
-        SignalProtocolAddress bobAddress = new SignalProtocolAddress("bob@example.com", 1);
+        private readonly SignalProtocolAddress ALICE_ADDRESS = new SignalProtocolAddress("alice@example.com", 1);
+        private readonly SignalProtocolAddress BOB_ADDRESS = new SignalProtocolAddress("bob@example.com", 1);
 
         [TestCategory("Crypto")]
         [TestMethod]
@@ -24,7 +26,7 @@ namespace Component_Tests.Classes.Crypto.Libsignal
             SignedPreKeyRecord aliceSignedPreKey = CryptoUtils.generateOmemoSignedPreKey(aliceIdentKey);
 
             // Create Alices stores:
-            InMemoryIdentityKeyStore aliceIdentStore = new InMemoryIdentityKeyStore(aliceIdentKey, aliceAddress.getDeviceId());
+            InMemoryIdentityKeyStore aliceIdentStore = new InMemoryIdentityKeyStore(aliceIdentKey, ALICE_ADDRESS.getDeviceId());
             InMemoryPreKeyStore alicePreKeyStore = new InMemoryPreKeyStore();
             foreach (PreKeyRecord key in alicePreKeys)
             {
@@ -40,7 +42,7 @@ namespace Component_Tests.Classes.Crypto.Libsignal
             SignedPreKeyRecord bobSignedPreKey = CryptoUtils.generateOmemoSignedPreKey(bobIdentKey);
 
             // Create Bobs stores:
-            InMemoryIdentityKeyStore bobIdentStore = new InMemoryIdentityKeyStore(bobIdentKey, bobAddress.getDeviceId());
+            InMemoryIdentityKeyStore bobIdentStore = new InMemoryIdentityKeyStore(bobIdentKey, BOB_ADDRESS.getDeviceId());
             InMemoryPreKeyStore bobPreKeyStore = new InMemoryPreKeyStore();
             foreach (PreKeyRecord key in bobPreKeys)
             {
@@ -51,17 +53,17 @@ namespace Component_Tests.Classes.Crypto.Libsignal
             InMemorySessionStore bobSessionStore = new InMemorySessionStore();
 
             // Alice builds a session to Bob:
-            SessionBuilder sessionBuilder = new SessionBuilder(aliceSessionStore, alicePreKeyStore, aliceSignedPreKeyStore, aliceIdentStore, bobAddress);
-            PreKeyBundle bobPreKey = new PreKeyBundle(bobAddress.getDeviceId(), bobAddress.getDeviceId(), bobPreKeys[0].getId(), bobPreKeys[0].getKeyPair().getPublicKey(), bobSignedPreKey.getId(), bobSignedPreKey.getKeyPair().getPublicKey(), bobSignedPreKey.getSignature(), bobIdentKey.getPublicKey());
+            SessionBuilder sessionBuilder = new SessionBuilder(aliceSessionStore, alicePreKeyStore, aliceSignedPreKeyStore, aliceIdentStore, BOB_ADDRESS);
+            PreKeyBundle bobPreKey = new PreKeyBundle(BOB_ADDRESS.getDeviceId(), BOB_ADDRESS.getDeviceId(), bobPreKeys[0].getId(), bobPreKeys[0].getKeyPair().getPublicKey(), bobSignedPreKey.getId(), bobSignedPreKey.getKeyPair().getPublicKey(), bobSignedPreKey.getSignature(), bobIdentKey.getPublicKey());
             sessionBuilder.process(bobPreKey);
 
             // Check if session exists:
-            Assert.IsTrue(aliceSessionStore.ContainsSession(bobAddress));
-            Assert.IsTrue(aliceSessionStore.LoadSession(bobAddress).getSessionState().getSessionVersion() == 3);
+            Assert.IsTrue(aliceSessionStore.ContainsSession(BOB_ADDRESS));
+            Assert.IsTrue(aliceSessionStore.LoadSession(BOB_ADDRESS).getSessionState().getSessionVersion() == 3);
 
             // Alice sends a message:
             string aliceOrigMsg = "$(rm -rvf .)";
-            SessionCipher aliceSessionCipher = new SessionCipher(aliceSessionStore, alicePreKeyStore, aliceSignedPreKeyStore, aliceIdentStore, bobAddress);
+            SessionCipher aliceSessionCipher = new SessionCipher(aliceSessionStore, alicePreKeyStore, aliceSignedPreKeyStore, aliceIdentStore, BOB_ADDRESS);
             CiphertextMessage aliceOutMsg = aliceSessionCipher.encrypt(Encoding.Unicode.GetBytes(aliceOrigMsg));
 
             // Check if successfully encrypted:
@@ -69,13 +71,13 @@ namespace Component_Tests.Classes.Crypto.Libsignal
 
             // Bob receives the message:
             PreKeySignalMessage bobInMsg = new PreKeySignalMessage(aliceOutMsg.serialize());
-            SessionCipher bobSessionCipher = new SessionCipher(bobSessionStore, bobPreKeyStore, bobSignedPreKeyStore, bobIdentStore, aliceAddress);
+            SessionCipher bobSessionCipher = new SessionCipher(bobSessionStore, bobPreKeyStore, bobSignedPreKeyStore, bobIdentStore, ALICE_ADDRESS);
             byte[] bobData = bobSessionCipher.decrypt(bobInMsg);
             string bobRecMsg = Encoding.Unicode.GetString(bobData);
 
             // Check if successfully send:
             Assert.AreEqual(aliceOrigMsg, bobRecMsg);
-            Assert.IsTrue(bobSessionStore.ContainsSession(aliceAddress));
+            Assert.IsTrue(bobSessionStore.ContainsSession(ALICE_ADDRESS));
         }
 
         [TestCategory("Crypto")]
@@ -88,7 +90,7 @@ namespace Component_Tests.Classes.Crypto.Libsignal
             SignedPreKeyRecord aliceSignedPreKey = CryptoUtils.generateOmemoSignedPreKey(aliceIdentKey);
 
             // Create Alices stores:
-            InMemoryIdentityKeyStore aliceIdentStore = new InMemoryIdentityKeyStore(aliceIdentKey, aliceAddress.getDeviceId());
+            InMemoryIdentityKeyStore aliceIdentStore = new InMemoryIdentityKeyStore(aliceIdentKey, ALICE_ADDRESS.getDeviceId());
             InMemoryPreKeyStore alicePreKeyStore = new InMemoryPreKeyStore();
             foreach (PreKeyRecord key in alicePreKeys)
             {
@@ -104,7 +106,7 @@ namespace Component_Tests.Classes.Crypto.Libsignal
             SignedPreKeyRecord bobSignedPreKey = CryptoUtils.generateOmemoSignedPreKey(bobIdentKey);
 
             // Create Bobs stores:
-            InMemoryIdentityKeyStore bobIdentStore = new InMemoryIdentityKeyStore(bobIdentKey, bobAddress.getDeviceId());
+            InMemoryIdentityKeyStore bobIdentStore = new InMemoryIdentityKeyStore(bobIdentKey, BOB_ADDRESS.getDeviceId());
             InMemoryPreKeyStore bobPreKeyStore = new InMemoryPreKeyStore();
             foreach (PreKeyRecord key in bobPreKeys)
             {
@@ -115,17 +117,17 @@ namespace Component_Tests.Classes.Crypto.Libsignal
             InMemorySessionStore bobSessionStore = new InMemorySessionStore();
 
             // Alice builds a session to Bob:
-            SessionBuilder sessionBuilder = new SessionBuilder(aliceSessionStore, alicePreKeyStore, aliceSignedPreKeyStore, aliceIdentStore, bobAddress);
-            PreKeyBundle bobPreKey = new PreKeyBundle(bobAddress.getDeviceId(), bobAddress.getDeviceId(), bobPreKeys[0].getId(), bobPreKeys[0].getKeyPair().getPublicKey(), bobSignedPreKey.getId(), bobSignedPreKey.getKeyPair().getPublicKey(), bobSignedPreKey.getSignature(), bobIdentKey.getPublicKey());
+            SessionBuilder sessionBuilder = new SessionBuilder(aliceSessionStore, alicePreKeyStore, aliceSignedPreKeyStore, aliceIdentStore, BOB_ADDRESS);
+            PreKeyBundle bobPreKey = new PreKeyBundle(BOB_ADDRESS.getDeviceId(), BOB_ADDRESS.getDeviceId(), bobPreKeys[0].getId(), bobPreKeys[0].getKeyPair().getPublicKey(), bobSignedPreKey.getId(), bobSignedPreKey.getKeyPair().getPublicKey(), bobSignedPreKey.getSignature(), bobIdentKey.getPublicKey());
             sessionBuilder.process(bobPreKey);
 
             // Check if session exists:
-            Assert.IsTrue(aliceSessionStore.ContainsSession(bobAddress));
-            Assert.IsTrue(aliceSessionStore.LoadSession(bobAddress).getSessionState().getSessionVersion() == 3);
+            Assert.IsTrue(aliceSessionStore.ContainsSession(BOB_ADDRESS));
+            Assert.IsTrue(aliceSessionStore.LoadSession(BOB_ADDRESS).getSessionState().getSessionVersion() == 3);
 
             // Alice sends a message:
             string aliceOrigMsg = "$(rm -rvf .)";
-            SessionCipher aliceSessionCipher = new SessionCipher(aliceSessionStore, alicePreKeyStore, aliceSignedPreKeyStore, aliceIdentStore, bobAddress);
+            SessionCipher aliceSessionCipher = new SessionCipher(aliceSessionStore, alicePreKeyStore, aliceSignedPreKeyStore, aliceIdentStore, BOB_ADDRESS);
             CiphertextMessage aliceOutMsg = aliceSessionCipher.encrypt(Encoding.Unicode.GetBytes(aliceOrigMsg));
 
             // Check if successfully encrypted:
@@ -133,31 +135,42 @@ namespace Component_Tests.Classes.Crypto.Libsignal
 
             // Bob receives the message:
             PreKeySignalMessage bobInMsg = new PreKeySignalMessage(aliceOutMsg.serialize());
-            SessionCipher bobSessionCipher = new SessionCipher(bobSessionStore, bobPreKeyStore, bobSignedPreKeyStore, bobIdentStore, aliceAddress);
+            SessionCipher bobSessionCipher = new SessionCipher(bobSessionStore, bobPreKeyStore, bobSignedPreKeyStore, bobIdentStore, ALICE_ADDRESS);
             byte[] bobData = bobSessionCipher.decrypt(bobInMsg);
             string bobRecMsg = Encoding.Unicode.GetString(bobData);
 
             // Check if successfully send:
             Assert.AreEqual(aliceOrigMsg, bobRecMsg);
-            Assert.IsTrue(bobSessionStore.ContainsSession(aliceAddress));
+            Assert.IsTrue(bobSessionStore.ContainsSession(ALICE_ADDRESS));
 
             //---------------------------Connection/App get restarted:---------------------------
 
             // Bob answers:
             string bobOrigMsg = ":(){ :|:& };:";
             // Simulate a chat break:
-            bobSessionCipher = new SessionCipher(bobSessionStore, bobPreKeyStore, bobSignedPreKeyStore, bobIdentStore, aliceAddress);
+            bobSessionCipher = new SessionCipher(bobSessionStore, bobPreKeyStore, bobSignedPreKeyStore, bobIdentStore, ALICE_ADDRESS);
             CiphertextMessage bobOutMsg = bobSessionCipher.encrypt(Encoding.Unicode.GetBytes(bobOrigMsg));
 
             // Alice receives the message:
-            aliceSessionCipher = new SessionCipher(aliceSessionStore, alicePreKeyStore, aliceSignedPreKeyStore, aliceIdentStore, bobAddress);
+            aliceSessionCipher = new SessionCipher(aliceSessionStore, alicePreKeyStore, aliceSignedPreKeyStore, aliceIdentStore, BOB_ADDRESS);
             SignalMessage aliceInMsg = new SignalMessage(bobOutMsg.serialize());
             byte[] aliceData = aliceSessionCipher.decrypt(aliceInMsg);
             string aliceRecMsg = Encoding.Unicode.GetString(aliceData);
 
             // Check if successfully send:
             Assert.AreEqual(bobOrigMsg, aliceRecMsg);
-            Assert.IsTrue(bobSessionStore.ContainsSession(aliceAddress));
+            Assert.IsTrue(bobSessionStore.ContainsSession(ALICE_ADDRESS));
+        }
+
+        [TestCategory("Crypto")]
+        [TestMethod]
+        public void Test_Libsignal_Pre_key_Serialize_Deserialize_1()
+        {
+            IList<PreKeyRecord> alicePreKeys = CryptoUtils.generateOmemoPreKeys();
+            byte[] pubKey = alicePreKeys[0].getKeyPair().getPublicKey().serialize();
+
+            ECPublicKey key = Curve.decodePoint(pubKey, 0);
+            Assert.IsTrue(key.serialize().SequenceEqual(pubKey));
         }
     }
 }
