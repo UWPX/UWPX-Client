052ba29a232665cd3571a8c73d4dc10b45e32e5e
COM8 <sauter.fabian@mailbox.org>
2018-11-13 16:12:29 +0100

    Added powershell build scripts

---
+++ Build/Build_All.ps1
@@ -0,0 +1,13 @@
+﻿#$project="..\UWP XMPP Client\UWP XMPP Client.csproj"
+$project="..\UWP_XMPP_Client.sln"
+$parasBuild="/p:configuration=debug /p:Platform=x86|x64|ARM"
+$parasClean="/t:clean"
+$msBuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+
+# First clean the project:
+# & $msBuild $project $parasClean
+
+# Then Build the project:
+& $msBuild $project /p:configuration=debug /p:Platform=ARM
+#& $msBuild $project /p:configuration=debug /p:Platform=x86
+#& $msBuild $project /p:configuration=debug /p:Platform=x64
---
+++ Build/Build_CPP.ps1
@@ -0,0 +1,12 @@
+﻿$project="..\AES_GCM\AES_GCM.vcxproj"
+$parasBuild="/p:configuration=debug /p:Platform=ARM"
+$parasClean="/t:clean"
+$msBuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+
+# First clean the project:
+#& $msBuild $project $parasClean
+
+# Then Build the project:
+& $msBuild $project /p:configuration=debug /p:Platform=ARM
+& $msBuild $project /p:configuration=debug /p:Platform=x86
+& $msBuild $project /p:configuration=debug /p:Platform=x64
