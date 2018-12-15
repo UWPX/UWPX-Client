50e80f81d3cc7d564f8f51a00609feb854a4d6bc
COM8 <sauter.fabian@mailbox.org>
2018-12-15 18:22:41 +0100

    Started adding the Chats page

---
+++ UWPX_UI/Controls/CustomTitleBarControl.xaml.cs
@@ -0,0 +1,77 @@
+﻿using UWPX_UI_Context.Classes;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPX_UI.Controls
+{
+    public sealed partial class CustomTitleBarControl : UserControl
+    {
+        //--------------------------------------------------------Attributes:-----------------------------------------------------------------\\
+        #region --Attributes--
+
+
+        #endregion
+        //--------------------------------------------------------Constructor:----------------------------------------------------------------\\
+        #region --Constructors--
+        public CustomTitleBarControl()
+        {
+            this.InitializeComponent();
+            InitTitleBar();
+        }
+
+        #endregion
+        //--------------------------------------------------------Set-, Get- Methods:---------------------------------------------------------\\
+        #region --Set-, Get- Methods--
+
+
+        #endregion
+        //--------------------------------------------------------Misc Methods:---------------------------------------------------------------\\
+        #region --Misc Methods (Public)--
+
+
+        #endregion
+
+        #region --Misc Methods (Private)--
+        private void InitTitleBar()
+        {
+            if (UiUtils.IsApplicationViewApiAvailable() && !UiUtils.IsRunningOnMobileDevice())
+            {
+                // Set XAML element as a draggable region.
+                CoreApplicationViewTitleBar titleBar = CoreApplication.GetCurrentView().TitleBar;
+                UpdateTitleBarLayout(titleBar);
+                Window.Current.SetTitleBar(titleBar_grid);
+                titleBar.LayoutMetricsChanged += TitleBar_LayoutMetricsChanged;
+            }
+        }
+
+        private void TitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            UpdateTitleBarLayout(sender);
+        }
+
+        private void UpdateTitleBarLayout(CoreApplicationViewTitleBar titleBar)
+        {
+            // Get the size of the caption controls area and back button 
+            // (returned in logical pixels), and move your content around as necessary.
+            leftPaddingColumn.Width = new GridLength(titleBar.SystemOverlayLeftInset);
+            rightPaddingColumn.Width = new GridLength(titleBar.SystemOverlayRightInset);
+            titleBar_grid.Margin = new Thickness(0, -1, 0, 0);
+
+            // Update title bar control size as needed to account for system size changes.
+            titleBar_grid.Height = titleBar.Height;
+        }
+
+        #endregion
+
+        #region --Misc Methods (Protected)--
+
+
+        #endregion
+        //--------------------------------------------------------Events:---------------------------------------------------------------------\\
+        #region --Events--
+
+
+        #endregion
+    }
+}
--- UWPX_UI_Context/Classes/UiUtils.cs
+++ UWPX_UI_Context/Classes/UiUtils.cs
@@ -1,19 +1,26 @@
-﻿using Windows.UI.Xaml;
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Foundation.Metadata;
+using Windows.System.Profile;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 using XMPP_API.Classes;
 
 namespace UWPX_UI_Context.Classes
 {
-    static class UiUtils
+    public static class UiUtils
     {
         //--------------------------------------------------------Attributes:-----------------------------------------------------------------\\
         #region --Attributes--
-
+        private static TaskCompletionSource<ContentDialog> contentDialogShowRequest;
 
         #endregion
         //--------------------------------------------------------Set-, Get- Methods:---------------------------------------------------------\\
         #region --Set-, Get- Methods--
-        public static SolidColorBrush getPresenceBrush(Presence presence)
+        public static SolidColorBrush GetPresenceBrush(Presence presence)
         {
             switch (presence)
             {
@@ -38,10 +45,101 @@ namespace UWPX_UI_Context.Classes
             }
         }
 
+        public static bool IsDarkThemeActive()
+        {
+            return Application.Current.RequestedTheme == ApplicationTheme.Dark;
+        }
+
+        /// <summary>
+        /// Checks whether the current device is a Windows Mobile device.
+        /// </summary>
+        public static bool IsRunningOnMobileDevice()
+        {
+            return AnalyticsInfo.VersionInfo.DeviceFamily.Equals("Windows.Mobile");
+        }
+
+        public static bool IsApplicationViewApiAvailable()
+        {
+            return ApiInformation.IsTypePresent("Windows.UI.ViewManagement.ApplicationView");
+        }
+
+        public static bool IsStatusBarApiAvailable()
+        {
+            return ApiInformation.IsTypePresent("Windows.UI.ViewManagement.StatusBar");
+        }
+
         #endregion
         //--------------------------------------------------------Misc Methods:---------------------------------------------------------------\\
         #region --Misc Methods (Public)--
+        /// <summary>
+        /// Shows the given content dialog on the screen, if no other dialog is shown right now.
+        /// If there is an other one being shown, the dialog will be shown afterwards.
+        /// </summary>
+        /// <param name="dialog">The dialog that should get shown.</param>
+        public static async Task<ContentDialogResult> ShowDialogAsync(ContentDialog dialog)
+        {
+            // Make sure it gets invoked by the UI thread:
+            if (!Window.Current.Dispatcher.HasThreadAccess)
+            {
+                throw new InvalidOperationException("This method can only be invoked from UI thread.");
+            }
 
+            while (contentDialogShowRequest != null)
+            {
+                await contentDialogShowRequest.Task;
+            }
+
+            contentDialogShowRequest = new TaskCompletionSource<ContentDialog>();
+            var result = await dialog.ShowAsync();
+            contentDialogShowRequest.SetResult(dialog);
+            contentDialogShowRequest = null;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Launches the default application for the given Uri.
+        /// </summary>
+        /// <param name="url">The Uri that defines the application that should get launched.</param>
+        /// <returns>Returns true on success.</returns>
+        public static async Task<bool> LaunchUriAsync(Uri url)
+        {
+            return await Windows.System.Launcher.LaunchUriAsync(url);
+        }
+
+        public static void SetupWindow(Application application)
+        {
+            // PC:
+            if (IsApplicationViewApiAvailable())
+            {
+                ApplicationView appView = ApplicationView.GetForCurrentView();
+
+                // Dye title:
+                appView.TitleBar.BackgroundColor = ((Microsoft.UI.Xaml.Media.AcrylicBrush)application.Resources["AppBackgroundAcrylicWindowBrush"]).TintColor;
+
+                //Dye title bar buttons:
+                bool isDarkTheme = IsDarkThemeActive();
+                appView.TitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+                appView.TitleBar.ButtonInactiveForegroundColor = (isDarkTheme) ? Colors.White : Colors.Black;
+                appView.TitleBar.ButtonBackgroundColor = Colors.Transparent;
+                appView.TitleBar.ButtonForegroundColor = (isDarkTheme) ? Colors.White : Colors.Black;
+
+                // Extend window:
+                Windows.ApplicationModel.Core.CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            }
+
+            // Mobile:
+            if (IsStatusBarApiAvailable())
+            {
+
+                var statusBar = StatusBar.GetForCurrentView();
+                if (statusBar != null)
+                {
+                    statusBar.BackgroundColor = ((AcrylicBrush)application.Resources["AppBackgroundAcrylicWindowBrush"]).TintColor;
+                    statusBar.BackgroundOpacity = 1;
+                }
+            }
+        }
 
         #endregion
 
