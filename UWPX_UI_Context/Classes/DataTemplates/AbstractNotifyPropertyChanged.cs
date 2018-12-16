5f0a201c25f84fc99a223767d81278d06d2ad1c0
COM8 <sauter.fabian@mailbox.org>
2018-12-16 17:22:59 +0100

    Added more ChatMasterControl related properties

---
+++ UWPX_UI_Context/Classes/DataTemplates/AbstractNotifyPropertyChanged.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UWPX_UI_Context.Classes.DataTemplates
+{
+    public abstract class AbstractNotifyPropertyChanged : INotifyPropertyChanged
+    {
+        //--------------------------------------------------------Attributes:-----------------------------------------------------------------\\
+        #region --Attributes--
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+        //--------------------------------------------------------Constructor:----------------------------------------------------------------\\
+        #region --Constructors--
+
+
+        #endregion
+        //--------------------------------------------------------Set-, Get- Methods:---------------------------------------------------------\\
+        #region --Set-, Get- Methods--
+
+
+        #endregion
+        //--------------------------------------------------------Misc Methods:---------------------------------------------------------------\\
+        #region --Misc Methods (Public)--
+
+
+        #endregion
+
+        #region --Misc Methods (Private)--
+
+
+        #endregion
+
+        #region --Misc Methods (Protected)--
+        protected async void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            await UiUtils.CallDispatcherAsync(() => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name)));
+        }
+
+        #endregion
+        //--------------------------------------------------------Events:---------------------------------------------------------------------\\
+        #region --Events--
+
+
+        #endregion
+    }
+}
