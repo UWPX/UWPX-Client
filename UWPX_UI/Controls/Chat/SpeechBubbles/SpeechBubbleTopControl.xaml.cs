c29ccd87153e60b395befa945b5008c161454796
COM8 <sauter.fabian@mailbox.org>
2018-12-23 11:49:07 +0100

    More SpeechBubbleControl preparations

--- UWPX_UI/Controls/Chat/SpeechBubbles/SpeechBubbleTopControl.xaml.cs
+++ UWPX_UI/Controls/Chat/SpeechBubbles/SpeechBubbleTopControl.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using UWPX_UI_Context.Classes.DataTemplates;
+using Windows.Foundation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace UWPX_UI.Controls.Chat.SpeechBubbles
 {
@@ -14,6 +16,8 @@ namespace UWPX_UI.Controls.Chat.SpeechBubbles
             set { SetValue(ChatMessageProperty, value); }
         }
         public static readonly DependencyProperty ChatMessageProperty = DependencyProperty.Register(nameof(ChatMessage), typeof(ChatMessageDataTemplate), typeof(SpeechBubbleTopControl), new PropertyMetadata(null));
+
+        private readonly PointCollection SPEECH_BUBBLE_POINTS = new PointCollection();
 
         #endregion
         //--------------------------------------------------------Constructor:----------------------------------------------------------------\\
@@ -36,7 +40,21 @@ namespace UWPX_UI.Controls.Chat.SpeechBubbles
         #endregion
 
         #region --Misc Methods (Private)--
-
+        private void UpdateSpeechBubbleSize(Size size)
+        {
+            double width = size.Width < MinWidth ? MinWidth : size.Width;
+            double height = (size.Height < MinHeight ? MinHeight : size.Height) + 10;
+            /*speechBubble_poly.Points = new PointCollection()
+            {
+                new Point(0, 10),
+                new Point(10, 10),
+                new Point(10, 0),
+                new Point(20, 10),
+                new Point(width, 10),
+                new Point(width, height),
+                new Point(0, height)
+            };*/
+        }
 
         #endregion
 
@@ -46,8 +64,15 @@ namespace UWPX_UI.Controls.Chat.SpeechBubbles
         #endregion
         //--------------------------------------------------------Events:---------------------------------------------------------------------\\
         #region --Events--
-
+        private void SpeechBubbleContentControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+        }
 
         #endregion
+
+        private void SpeechBubble_poly_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            //UpdateSpeechBubbleSize(e.NewSize);
+        }
     }
 }
---
+++ UWPX_UI_Context/Classes/DataContext/SpeechBubbleContentContext.cs
@@ -0,0 +1,62 @@
+﻿using UWPX_UI_Context.Classes.DataTemplates;
+using Windows.UI.Xaml;
+
+namespace UWPX_UI_Context.Classes.DataContext
+{
+    public class SpeechBubbleContentContext
+    {
+        //--------------------------------------------------------Attributes:-----------------------------------------------------------------\\
+        #region --Attributes--
+        public readonly SpeechBubbleContentDataTemplate MODEL = new SpeechBubbleContentDataTemplate();
+
+        #endregion
+        //--------------------------------------------------------Constructor:----------------------------------------------------------------\\
+        #region --Constructors--
+
+
+        #endregion
+        //--------------------------------------------------------Set-, Get- Methods:---------------------------------------------------------\\
+        #region --Set-, Get- Methods--
+
+
+        #endregion
+        //--------------------------------------------------------Misc Methods:---------------------------------------------------------------\\
+        #region --Misc Methods (Public)--
+        public void UpdateView(DependencyPropertyChangedEventArgs args)
+        {
+            if (args.OldValue is ChatMessageDataTemplate oldChatMessage)
+            {
+                oldChatMessage.PropertyChanged += ChatMessage_PropertyChanged;
+            }
+
+            if (args.OldValue is ChatMessageDataTemplate newChatMessage)
+            {
+                newChatMessage.PropertyChanged += ChatMessage_PropertyChanged;
+                MODEL.UpdateView(newChatMessage.Chat, newChatMessage.Message);
+            }
+        }
+
+        #endregion
+
+        #region --Misc Methods (Private)--
+
+
+        #endregion
+
+        #region --Misc Methods (Protected)--
+
+
+        #endregion
+        //--------------------------------------------------------Events:---------------------------------------------------------------------\\
+        #region --Events--
+        private void ChatMessage_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (sender is ChatMessageDataTemplate chatMessage)
+            {
+                MODEL.UpdateView(chatMessage.Chat, chatMessage.Message);
+            }
+        }
+
+        #endregion
+    }
+}
