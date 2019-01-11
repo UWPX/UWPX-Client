da970eaeeea76c33eae3bdc920c8a5562bece97c
COM8 <sauter.fabian@mailbox.org>
2019-01-11 21:19:15 +0100

    Finished log level selection

---
+++ UWPX_UI_Context/Classes/DataTemplates/LogLevelDataTemplate.cs
@@ -0,0 +1,48 @@
+﻿using Logging;
+
+namespace UWPX_UI_Context.Classes.DataTemplates
+{
+    public sealed class LogLevelDataTemplate : AbstractDataTemplate
+    {
+        //--------------------------------------------------------Attributes:-----------------------------------------------------------------\\
+        #region --Attributes--
+        private LogLevel _LogLevel;
+        public LogLevel LogLevel
+        {
+            get { return _LogLevel; }
+            set { SetProperty(ref _LogLevel, value); }
+        }
+
+        #endregion
+        //--------------------------------------------------------Constructor:----------------------------------------------------------------\\
+        #region --Constructors--
+
+
+        #endregion
+        //--------------------------------------------------------Set-, Get- Methods:---------------------------------------------------------\\
+        #region --Set-, Get- Methods--
+
+
+        #endregion
+        //--------------------------------------------------------Misc Methods:---------------------------------------------------------------\\
+        #region --Misc Methods (Public)--
+
+
+        #endregion
+
+        #region --Misc Methods (Private)--
+
+
+        #endregion
+
+        #region --Misc Methods (Protected)--
+
+
+        #endregion
+        //--------------------------------------------------------Events:---------------------------------------------------------------------\\
+        #region --Events--
+
+
+        #endregion
+    }
+}
