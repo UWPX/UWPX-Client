3cad70f1095e87171ad60efc30bac82e0230b520
COM8 <sauter.fabian@mailbox.org>
2019-01-17 18:24:38 +0100

    Started adding the accounts settings page

---
+++ UWPX_UI_Context/Classes/DataTemplates/AccountSettingsPageDataTemplate.cs
@@ -0,0 +1,41 @@
+﻿namespace UWPX_UI_Context.Classes.DataTemplates
+{
+    public sealed class AccountSettingsPageDataTemplate : AbstractDataTemplate
+    {
+        //--------------------------------------------------------Attributes:-----------------------------------------------------------------\\
+        #region --Attributes--
+
+
+        #endregion
+        //--------------------------------------------------------Constructor:----------------------------------------------------------------\\
+        #region --Constructors--
+
+
+        #endregion
+        //--------------------------------------------------------Set-, Get- Methods:---------------------------------------------------------\\
+        #region --Set-, Get- Methods--
+
+
+        #endregion
+        //--------------------------------------------------------Misc Methods:---------------------------------------------------------------\\
+        #region --Misc Methods (Public)--
+
+
+        #endregion
+
+        #region --Misc Methods (Private)--
+
+
+        #endregion
+
+        #region --Misc Methods (Protected)--
+
+
+        #endregion
+        //--------------------------------------------------------Events:---------------------------------------------------------------------\\
+        #region --Events--
+
+
+        #endregion
+    }
+}
