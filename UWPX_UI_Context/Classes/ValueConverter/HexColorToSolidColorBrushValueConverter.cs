9196ef73b44197d527e048ee2c4eaea56d2b0a77
COM8 <sauter.fabian@mailbox.org>
2019-01-19 17:32:48 +0100

    More UI work done on the accounts settings page

---
+++ UWPX_UI_Context/Classes/ValueConverter/HexColorToSolidColorBrushValueConverter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace UWPX_UI_Context.Classes.ValueConverter
+{
+    public sealed class HexColorToSolidColorBrushValueConverter : IValueConverter
+    {
+        //--------------------------------------------------------Attributes:-----------------------------------------------------------------\\
+        #region --Attributes--
+
+
+        #endregion
+        //--------------------------------------------------------Constructor:----------------------------------------------------------------\\
+        #region --Constructors--
+
+
+        #endregion
+        //--------------------------------------------------------Set-, Get- Methods:---------------------------------------------------------\\
+        #region --Set-, Get- Methods--
+
+
+        #endregion
+        //--------------------------------------------------------Misc Methods:---------------------------------------------------------------\\
+        #region --Misc Methods (Public)--
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is string s && UiUtils.IsHexColor(s))
+            {
+                return UiUtils.HexStringToBrush(s);
+            }
+            return new SolidColorBrush(Colors.Transparent);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is SolidColorBrush b)
+            {
+                return UiUtils.ColorToHexString(b.Color);
+            }
+            return "";
+        }
+
+        #endregion
+
+        #region --Misc Methods (Private)--
+
+
+        #endregion
+
+        #region --Misc Methods (Protected)--
+
+
+        #endregion
+        //--------------------------------------------------------Events:---------------------------------------------------------------------\\
+        #region --Events--
+
+
+        #endregion
+    }
+}
