816ea9c39a55bbd2cb615e3df9493d00b59c4105
COM8 <sauter.fabian@mailbox.org>
2019-02-10 13:08:13 +0100

    Finished the account info OMEMO control

--- UWPX_UI/Controls/OMEMO/OmemoFingerprintControl.xaml.cs
+++ UWPX_UI/Controls/OMEMO/OmemoFingerprintControl.xaml.cs
@@ -1,11 +1,10 @@
-﻿using UWPX_UI_Context.Classes.DataContext.Controls;
-using UWPX_UI_Context.Classes.DataTemplates;
+﻿using UWPX_UI_Context.Classes.DataTemplates;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
-namespace UWPX_UI.Controls
+namespace UWPX_UI.Controls.OMEMO
 {
-    public sealed partial class OmemoDeviceListControl : UserControl
+    public sealed partial class OmemoFingerprintControl : UserControl
     {
         //--------------------------------------------------------Attributes:-----------------------------------------------------------------\\
         #region --Attributes--
@@ -14,14 +13,12 @@ namespace UWPX_UI.Controls
             get { return (AccountDataTemplate)GetValue(AccountProperty); }
             set { SetValue(AccountProperty, value); }
         }
-        public static readonly DependencyProperty AccountProperty = DependencyProperty.Register(nameof(Account), typeof(AccountDataTemplate), typeof(OmemoDeviceListControl), new PropertyMetadata(null, OnAccountChanged));
-
-        public readonly OmemoDeviceListControlContext VIEW_MODEL = new OmemoDeviceListControlContext();
+        public static readonly DependencyProperty AccountProperty = DependencyProperty.Register(nameof(Account), typeof(AccountDataTemplate), typeof(OmemoFingerprintControl), new PropertyMetadata(null, OnAccountChanged));
 
         #endregion
         //--------------------------------------------------------Constructor:----------------------------------------------------------------\\
         #region --Constructors--
-        public OmemoDeviceListControl()
+        public OmemoFingerprintControl()
         {
             this.InitializeComponent();
         }
@@ -52,19 +49,9 @@ namespace UWPX_UI.Controls
         #endregion
         //--------------------------------------------------------Events:---------------------------------------------------------------------\\
         #region --Events--
-        private void Reset_ibtn_Click(IconProgressButtonControl sender, RoutedEventArgs args)
-        {
-            VIEW_MODEL.ResetOmemoDevices(Account.Client);
-        }
-
-        private void Refresh_ibtn_Click(object sender, RoutedEventArgs args)
-        {
-            VIEW_MODEL.RefreshOmemoDevices(Account.Client);
-        }
-
         private static void OnAccountChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            if (d is OmemoDeviceListControl omemoDeviceListControl)
+            if (d is OmemoFingerprintControl omemoFingerprintControl)
             {
                 omemoDeviceListControl.UpdateView(e);
             }
---
+++ UWPX_UI_Context/Classes/DataContext/Controls/OmemoFingerprintControlContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPX_UI_Context.Classes.DataContext.Controls
+{
+    class OmemoFingerprintControlContext
+    {
+        //--------------------------------------------------------Attributes:-----------------------------------------------------------------\\
+        #region --Attributes--
+
+
+        #endregion
+        //--------------------------------------------------------Constructor:----------------------------------------------------------------\\
+        #region --Constructors--
+
+
+        #endregion
+        //--------------------------------------------------------Set-, Get- Methods:---------------------------------------------------------\\
+        #region --Set-, Get- Methods--
+
+
+        #endregion
+        //--------------------------------------------------------Misc Methods:---------------------------------------------------------------\\
+        #region --Misc Methods (Public)--
+
+
+        #endregion
+
+        #region --Misc Methods (Private)--
+
+
+        #endregion
+
+        #region --Misc Methods (Protected)--
+
+
+        #endregion
+        //--------------------------------------------------------Events:---------------------------------------------------------------------\\
+        #region --Events--
+
+
+        #endregion
+    }
+}
