6e44a5eb935749a0330738549501354ace6377a8
COM8 <sauter.fabian@mailbox.org>
2019-03-15 11:02:51 +0100

    Added an indication when OMEMO messages are being encrypted

---
+++ UWPX_UI_Context/Classes/ValueConverter/MessageStateEncryptingVisabilityValueConverter.cs
@@ -0,0 +1,58 @@
+﻿using Data_Manager2.Classes;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace UWPX_UI_Context.Classes.ValueConverter
+{
+    public sealed class MessageStateEncryptingVisabilityValueConverter : IValueConverter
+    {
+        //--------------------------------------------------------Attributes:-----------------------------------------------------------------\\
+        #region --Attributes--
+
+
+        #endregion
+        //--------------------------------------------------------Constructor:----------------------------------------------------------------\\
+        #region --Constructors--
+
+
+        #endregion
+        //--------------------------------------------------------Set-, Get- Methods:---------------------------------------------------------\\
+        #region --Set-, Get- Methods--
+
+
+        #endregion
+        //--------------------------------------------------------Misc Methods:---------------------------------------------------------------\\
+        #region --Misc Methods (Public)--
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is MessageState state && state == MessageState.TO_ENCRYPT)
+            {
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region --Misc Methods (Private)--
+
+
+        #endregion
+
+        #region --Misc Methods (Protected)--
+
+
+        #endregion
+        //--------------------------------------------------------Events:---------------------------------------------------------------------\\
+        #region --Events--
+
+
+        #endregion
+    }
+}
