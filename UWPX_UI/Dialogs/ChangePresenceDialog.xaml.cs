b298d43e722517bb56980bb4c537671441610288
Fabian Sauter <sauter.fabian@mailbox.org>
2019-04-15 17:33:03 +0200

    Started replacing the change account presence dialog with a new one

---
+++ UWPX_UI/Dialogs/ChangePresenceDialog.xaml.cs
@@ -0,0 +1,50 @@
+﻿using UWPX_UI_Context.Classes.DataContext.Dialogs;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPX_UI.Dialogs
+{
+    public sealed partial class ChangePresenceDialog: ContentDialog
+    {
+        //--------------------------------------------------------Attributes:-----------------------------------------------------------------\\
+        #region --Attributes--
+        public readonly ChangePresenceDialogContext VIEW_MODEL = new ChangePresenceDialogContext();
+
+        #endregion
+        //--------------------------------------------------------Constructor:----------------------------------------------------------------\\
+        #region --Constructors--
+        public ChangePresenceDialog() => InitializeComponent();
+
+        #endregion
+        //--------------------------------------------------------Set-, Get- Methods:---------------------------------------------------------\\
+        #region --Set-, Get- Methods--
+
+
+        #endregion
+        //--------------------------------------------------------Misc Methods:---------------------------------------------------------------\\
+        #region --Misc Methods (Public)--
+
+
+        #endregion
+
+        #region --Misc Methods (Private)--
+
+
+        #endregion
+
+        #region --Misc Methods (Protected)--
+
+
+        #endregion
+        //--------------------------------------------------------Events:---------------------------------------------------------------------\\
+        #region --Events--
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+
+        #endregion
+    }
+}
