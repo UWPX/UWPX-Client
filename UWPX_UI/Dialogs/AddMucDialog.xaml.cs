668ccdd0a6319bc1ffb65e717b8b9917569ff1c8
Fabian Sauter <sauter.fabian@mailbox.org>
2019-04-25 19:00:54 +0200

    Started adding the add MUC dialog

---
+++ UWPX_UI/Dialogs/AddMucDialog.xaml.cs
@@ -0,0 +1,63 @@
+﻿using UWPX_UI_Context.Classes.DataContext.Dialogs;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPX_UI.Dialogs
+{
+    public sealed partial class AddMucDialog: ContentDialog
+    {
+        //--------------------------------------------------------Attributes:-----------------------------------------------------------------\\
+        #region --Attributes--
+        public readonly AddMucDialogContext VIEW_MODEL = new AddMucDialogContext();
+
+        #endregion
+        //--------------------------------------------------------Constructor:----------------------------------------------------------------\\
+        #region --Constructors--
+        public AddMucDialog()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+        //--------------------------------------------------------Set-, Get- Methods:---------------------------------------------------------\\
+        #region --Set-, Get- Methods--
+
+
+        #endregion
+        //--------------------------------------------------------Misc Methods:---------------------------------------------------------------\\
+        #region --Misc Methods (Public)--
+
+
+        #endregion
+
+        #region --Misc Methods (Private)--
+
+
+        #endregion
+
+        #region --Misc Methods (Protected)--
+
+
+        #endregion
+        //--------------------------------------------------------Events:---------------------------------------------------------------------\\
+        #region --Events--
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+
+        private void AccountSelectionControl_AddAccountClick(Controls.AccountSelectionControl sender, System.ComponentModel.CancelEventArgs args)
+        {
+            Hide();
+        }
+
+        private void AccountSelectionControl_AccountSelectionChanged(Controls.AccountSelectionControl sender, Classes.Events.AccountSelectionChangedEventArgs args)
+        {
+
+        }
+
+        #endregion
+    }
+}
