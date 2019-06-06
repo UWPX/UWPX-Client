794c2bbda0ed826d8e95cfceae29a6cf9b8daf02
Fabian Sauter <sauter.fabian@mailbox.org>
2019-06-06 20:51:00 +0200

    Replaced the "Segoe MDL2 Assets" font with a newer one on build time.

--- UWPX_UI/Scripts/setupFont.ps1
+++ UWPX_UI/Scripts/setupFont.ps1
@@ -1,5 +1,14 @@
 $solPath = $args[0]
 
+if (Test-Path "env:RunningCI") {
+    Write-Output "Skipping font import."
+    exit 0
+}
+
 Write-Output "Copying font 'Segoe UI Emoji' (seguiemj.ttf)..."
 cp -force C:\Windows\Fonts\seguiemj.ttf $solPath\Assets\Fonts\seguiemj.ttf
+Write-Output "Font copied"
+
+Write-Output "Copying font 'Segoe MDL2 Assets' (segmdl2.ttf)..."
+cp -force C:\Windows\Fonts\segmdl2.ttf $solPath\Assets\Fonts\segmdl2.ttf
 Write-Output "Font copied"
