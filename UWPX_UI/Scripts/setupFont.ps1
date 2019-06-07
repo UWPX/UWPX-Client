d72086d047f06a8ab78921ad20285647c6403b8b
Fabian Sauter <sauter.fabian@mailbox.org>
2019-06-07 20:43:28 +0200

    Updated the way fonts get imported

--- UWPX_UI/Scripts/setupFont.ps1
+++ UWPX_UI/Scripts/setupFont.ps1
@@ -1,14 +1,13 @@
 $solPath = $args[0]
 
-if (Test-Path "env:RunningCI") {
-    Write-Output "Skipping font import."
+if (Test-Path "env:windir") {
+    Write-Output "Copying font 'Segoe UI Emoji' (seguiemj.ttf)..."
+    cp -force "$env:windir\Fonts\seguiemj.ttf" $solPath\Assets\Fonts\seguiemj.ttf
+    Write-Output "Font copied"
+
+    Write-Output "Copying font 'Segoe MDL2 Assets' (segmdl2.ttf)..."
+    cp -force "$env:windir\Fonts\segmdl2.ttf" $solPath\Assets\Fonts\segmdl2.ttf
+    Write-Output "Font copied"
     exit 0
 }
-
-Write-Output "Copying font 'Segoe UI Emoji' (seguiemj.ttf)..."
-cp -force C:\Windows\Fonts\seguiemj.ttf $solPath\Assets\Fonts\seguiemj.ttf
-Write-Output "Font copied"
-
-Write-Output "Copying font 'Segoe MDL2 Assets' (segmdl2.ttf)..."
-cp -force C:\Windows\Fonts\segmdl2.ttf $solPath\Assets\Fonts\segmdl2.ttf
-Write-Output "Font copied"
+Write-Output "Skipping font import. No 'windir'"
