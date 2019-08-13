5ccc1e1607e4bf4bbfc5436e7f1ffea9abaf4a22
Fabian Sauter <sauter.fabian@mailbox.org>
2019-08-13 10:38:52 +0200

    Not failing if fonts couldn't be found

--- UWPX_UI/Scripts/setupFont.ps1
+++ UWPX_UI/Scripts/setupFont.ps1
@@ -1,13 +1,27 @@
 $solPath = $args[0]
 
 if (Test-Path "env:windir") {
-    Write-Output "Copying font 'Segoe UI Emoji' (seguiemj.ttf)..."
-    cp -force "$env:windir\Fonts\seguiemj.ttf" $solPath\Assets\Fonts\seguiemj.ttf
-    Write-Output "Font copied"
 
-    Write-Output "Copying font 'Segoe MDL2 Assets' (segmdl2.ttf)..."
-    cp -force "$env:windir\Fonts\segmdl2.ttf" $solPath\Assets\Fonts\segmdl2.ttf
-    Write-Output "Font copied"
+	$segoeUiPath = "$env:windir\Fonts\seguiemj.ttf"
+    if(Test-Path $segoeUiPath) {
+		Write-Output "Copying font 'Segoe UI Emoji' (seguiemj.ttf)..."
+		cp -force $segoeUiPath $solPath\Assets\Fonts\seguiemj.ttf
+		Write-Output "'Segoe UI Emoji' copied."
+    }
+    else {
+	    Write-Warning "'Segoe UI Emoji' not found: $segoeUiPath"
+	}
+    
+
+    $segoeMDL2AssestsPath = "$env:windir\Fonts\seguiemj.ttf"
+	if(Test-Path $segoeMDL2AssestsPath) {
+		Write-Output "Copying font 'Segoe MDL2 Assets' (segmdl2.ttf)..."
+        cp -force $segoeMDL2AssestsPath $solPath\Assets\Fonts\segmdl2.ttf
+        Write-Output "'Segoe MDL2 Assets' copied."
+	}
+	else {
+	    Write-Warning "'Segoe UI Emoji' not found: $segoeUiPath"
+	}
     exit 0
 }
 Write-Output "Skipping font import. No 'windir'"
