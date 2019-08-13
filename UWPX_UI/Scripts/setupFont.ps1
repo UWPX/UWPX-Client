b8727d7c4d90e2e7d0ca3a88a569d3c1a1292f53
Fabian Sauter <sauter.fabian@mailbox.org>
2019-08-13 12:16:19 +0200

    Better file exists check for fonts script

--- UWPX_UI/Scripts/setupFont.ps1
+++ UWPX_UI/Scripts/setupFont.ps1
@@ -1,9 +1,8 @@
 $solPath = $args[0]
 
 if (Test-Path "env:windir") {
-
 	$segoeUiPath = "$env:windir\Fonts\seguiemj.ttf"
-    if(Test-Path $segoeUiPath) {
+    if([System.IO.File]::Exists($segoeUiPath)) {
 		Write-Output "Copying font 'Segoe UI Emoji' (seguiemj.ttf)..."
 		cp -force $segoeUiPath $solPath\Assets\Fonts\seguiemj.ttf
 		Write-Output "'Segoe UI Emoji' copied."
@@ -14,7 +13,7 @@ if (Test-Path "env:windir") {
     
 
     $segoeMDL2AssestsPath = "$env:windir\Fonts\seguiemj.ttf"
-	if(Test-Path $segoeMDL2AssestsPath) {
+	if([System.IO.File]::Exists($segoeMDL2AssestsPath)) {
 		Write-Output "Copying font 'Segoe MDL2 Assets' (segmdl2.ttf)..."
         cp -force $segoeMDL2AssestsPath $solPath\Assets\Fonts\segmdl2.ttf
         Write-Output "'Segoe MDL2 Assets' copied."
