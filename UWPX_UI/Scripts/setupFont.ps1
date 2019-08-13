91da54ea736cf007f60d111b58bc4fa81fa05c9f
Fabian Sauter <sauter.fabian@mailbox.org>
2019-08-13 13:23:15 +0200

    Updated the font copy script

--- UWPX_UI/Scripts/setupFont.ps1
+++ UWPX_UI/Scripts/setupFont.ps1
@@ -4,22 +4,23 @@ if (Test-Path "env:windir") {
 	$segoeUiPath = "$env:windir\Fonts\seguiemj.ttf"
     if([System.IO.File]::Exists($segoeUiPath)) {
 		Write-Output "Copying font 'Segoe UI Emoji' (seguiemj.ttf)..."
-		cp -force $segoeUiPath $solPath\Assets\Fonts\seguiemj.ttf
+		Copy-Item -Force -Path $segoeUiPath -Destination $solPath\Assets\Fonts\seguiemj.ttf
 		Write-Output "'Segoe UI Emoji' copied."
     }
     else {
-	    Write-Warning "'Segoe UI Emoji' not found: $segoeUiPath"
+		Write-Error "'Segoe UI Emoji' not found: $segoeUiPath"
+		exit 1
 	}
-    
 
-    $segoeMDL2AssestsPath = "$env:windir\Fonts\seguiemj.ttf"
+    $segoeMDL2AssestsPath = "$env:windir\Fonts\segmdl2.ttf"
 	if([System.IO.File]::Exists($segoeMDL2AssestsPath)) {
 		Write-Output "Copying font 'Segoe MDL2 Assets' (segmdl2.ttf)..."
-        cp -force $segoeMDL2AssestsPath $solPath\Assets\Fonts\segmdl2.ttf
+		Copy-Item -Force -Path $segoeMDL2AssestsPath -Destination $solPath\Assets\Fonts\segmdl2.ttf
         Write-Output "'Segoe MDL2 Assets' copied."
 	}
 	else {
-	    Write-Warning "'Segoe UI Emoji' not found: $segoeUiPath"
+		Write-Error "'Segoe UI Emoji' not found: $segoeUiPath"
+		exit 1
 	}
     exit 0
 }
