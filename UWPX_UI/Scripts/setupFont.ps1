c74ce1fca48e2ba1374a9add63389cb5d12e363b
Fabian Sauter <sauter.fabian@mailbox.org>
2019-08-18 21:36:33 +0200

    Creating fonts folder if it does not exist

--- UWPX_UI/Scripts/setupFont.ps1
+++ UWPX_UI/Scripts/setupFont.ps1
@@ -1,10 +1,13 @@
 $solPath = $args[0]
 
 if (Test-Path "env:windir") {
+	$fontsPath = "$solPath\Assets\Fonts"
+	New-Item -ItemType Directory -Force -Path $fontsPath
+
 	$segoeUiPath = "$env:windir\Fonts\seguiemj.ttf"
     if([System.IO.File]::Exists($segoeUiPath)) {
 		Write-Output "Copying font 'Segoe UI Emoji' (seguiemj.ttf)..."
-		Copy-Item -Force -Recurse -Path $segoeUiPath -Destination $solPath\Assets\Fonts\seguiemj.ttf
+		Copy-Item -Force -Recurse -Path $segoeUiPath -Destination $fontsPath\seguiemj.ttf
 		Write-Output "'Segoe UI Emoji' copied."
     }
     else {
@@ -15,7 +18,7 @@ if (Test-Path "env:windir") {
     $segoeMDL2AssestsPath = "$env:windir\Fonts\segmdl2.ttf"
 	if([System.IO.File]::Exists($segoeMDL2AssestsPath)) {
 		Write-Output "Copying font 'Segoe MDL2 Assets' (segmdl2.ttf)..."
-		Copy-Item -Force -Recurse -Path $segoeMDL2AssestsPath -Destination $solPath\Assets\Fonts\segmdl2.ttf
+		Copy-Item -Force -Recurse -Path $segoeMDL2AssestsPath -Destination $fontsPath\segmdl2.ttf
         Write-Output "'Segoe MDL2 Assets' copied."
 	}
 	else {
