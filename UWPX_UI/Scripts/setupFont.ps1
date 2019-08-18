53be71a83ef6a551074b76d170c5195aa3a6d6ff
Fabian Sauter <sauter.fabian@mailbox.org>
2019-08-18 20:11:35 +0200

    Forcing subfolder creation for font copy

--- UWPX_UI/Scripts/setupFont.ps1
+++ UWPX_UI/Scripts/setupFont.ps1
@@ -1,12 +1,10 @@
 $solPath = $args[0]
 
-tree $solPath
-
 if (Test-Path "env:windir") {
 	$segoeUiPath = "$env:windir\Fonts\seguiemj.ttf"
     if([System.IO.File]::Exists($segoeUiPath)) {
 		Write-Output "Copying font 'Segoe UI Emoji' (seguiemj.ttf)..."
-		Copy-Item -Force -Path $segoeUiPath -Destination $solPath\Assets\Fonts\seguiemj.ttf
+		Copy-Item -Force -Recurse -Path $segoeUiPath -Destination $solPath\Assets\Fonts\seguiemj.ttf
 		Write-Output "'Segoe UI Emoji' copied."
     }
     else {
@@ -17,7 +15,7 @@ if (Test-Path "env:windir") {
     $segoeMDL2AssestsPath = "$env:windir\Fonts\segmdl2.ttf"
 	if([System.IO.File]::Exists($segoeMDL2AssestsPath)) {
 		Write-Output "Copying font 'Segoe MDL2 Assets' (segmdl2.ttf)..."
-		Copy-Item -Force -Path $segoeMDL2AssestsPath -Destination $solPath\Assets\Fonts\segmdl2.ttf
+		Copy-Item -Force -Recurse -Path $segoeMDL2AssestsPath -Destination $solPath\Assets\Fonts\segmdl2.ttf
         Write-Output "'Segoe MDL2 Assets' copied."
 	}
 	else {
