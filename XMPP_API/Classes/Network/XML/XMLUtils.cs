744947a8c56291907a213941558eaf234d9fc7c5
Fabian Sauter <sauter.fabian@mailbox.org>
2020-09-27 18:40:33 +0200

    Cleanup

--- XMPP_API/Classes/Network/XML/XMLUtils.cs
+++ XMPP_API/Classes/Network/XML/XMLUtils.cs
@@ -39,7 +39,7 @@ namespace XMPP_API.Classes.Network.XML
         /// <returns>Returns null if node does not exist, else the node.</returns>
         public static XElement getNodeFromXElement(XElement node, in string name)
         {
-            if (node != null)
+            if (!(node is null))
             {
                 foreach (XElement n in node.Elements())
                 {
@@ -63,13 +63,13 @@ namespace XMPP_API.Classes.Network.XML
         /// <returns>Returns null if node does not exist, else the node.</returns>
         public static XmlNode getChildNode(XmlNode node, in string name, in string attribute, Regex attributeValueRegex)
         {
-            if (node != null && node.HasChildNodes)
+            if (!(node is null) && node.HasChildNodes)
             {
                 foreach (XmlNode n in node.ChildNodes)
                 {
                     if (n.Name.Equals(name))
                     {
-                        if (attribute != null)
+                        if (!(attribute is null))
                         {
                             if (n.Attributes[attribute] != null && attributeValueRegex.IsMatch(n.Attributes[attribute].Value))
                             {
@@ -97,13 +97,13 @@ namespace XMPP_API.Classes.Network.XML
         /// <returns>Returns null if node does not exist, else the node.</returns>
         public static XmlNode getChildNode(XmlNode node, in string name, in string attribute, in string attributeValue)
         {
-            if (node != null && node.HasChildNodes)
+            if (!(node is null) && node.HasChildNodes)
             {
                 foreach (XmlNode n in node.ChildNodes)
                 {
                     if (n.Name.Equals(name))
                     {
-                        if (attribute != null)
+                        if (!(attribute is null))
                         {
                             if (n.Attributes[attribute] != null && n.Attributes[attribute].Value.Equals(attributeValue))
                             {
@@ -151,11 +151,7 @@ namespace XMPP_API.Classes.Network.XML
         /// <returns>Returns the attribute if found else null.</returns>
         public static XmlAttribute getAttribute(XmlNode node, in string name)
         {
-            if (node.Attributes != null)
-            {
-                return node.Attributes[name];
-            }
-            return null;
+            return node.Attributes?[name];
         }
 
         /// <summary>
@@ -167,18 +163,16 @@ namespace XMPP_API.Classes.Network.XML
         /// <returns>The boolean representation of the given string.</returns>
         public static bool tryParseToBool(in string s)
         {
-            if (s != null)
+            if (!(s is null))
             {
-                int i = -1;
-                bool b = false;
-                if (int.TryParse(s, out i))
+                if (int.TryParse(s, out int i))
                 {
                     if (i == 1)
                     {
                         return true;
                     }
                 }
-                else if (bool.TryParse(s, out b))
+                else if (bool.TryParse(s, out bool b))
                 {
                     return b;
                 }
