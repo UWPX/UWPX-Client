3d779484a6a7c2391cc341719f20926ce073dc60
Fabian Sauter <sauter.fabian@mailbox.org>
2020-12-28 15:29:12 +0100

    Started porting the SQLite DB to the Microsoft entity framework

---
+++ Storage/Classes/Models/Omemo/OmemoSignedPreKey.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Classes.Models.Omemo
+{
+    /// <summary>
+    /// A signed pre key for XEP-0384 (OMEMO Encryption).
+    /// </summary>
+    public class OmemoSignedPreKey
+    {
+        //--------------------------------------------------------Attributes:-----------------------------------------------------------------\\
+        #region --Attributes--
+        [Key]
+        public string id { get; set; }
+        /// <summary>
+        /// The OMEMO signed pre key ID.
+        /// </summary>
+        [Required]
+        public uint keyId { get; set; }
+        /// <summary>
+        /// The actual signed pre key.
+        /// </summary>
+        [Required]
+        public byte[] key { get; set; }
+
+        #endregion
+        //--------------------------------------------------------Constructor:----------------------------------------------------------------\\
+        #region --Constructors--
+
+
+        #endregion
+        //--------------------------------------------------------Set-, Get- Methods:---------------------------------------------------------\\
+        #region --Set-, Get- Methods--
+
+
+        #endregion
+        //--------------------------------------------------------Misc Methods:---------------------------------------------------------------\\
+        #region --Misc Methods (Public)--
+
+
+        #endregion
+
+        #region --Misc Methods (Private)--
+
+
+        #endregion
+
+        #region --Misc Methods (Protected)--
+
+
+        #endregion
+        //--------------------------------------------------------Events:---------------------------------------------------------------------\\
+        #region --Events--
+
+
+        #endregion
+    }
+}
