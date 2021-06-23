ebef70c351f32314831823ea5f43a6e87864ac34
Fabian Sauter <sauter.fabian@mailbox.org>
2021-06-23 20:05:40 +0200

    Server provider badges

--- UWPX_UI_Context/Classes/DataTemplates/Controls/ServerProviderControlDataTemplate.cs
+++ UWPX_UI_Context/Classes/DataTemplates/Controls/ServerProviderControlDataTemplate.cs
@@ -6,7 +6,68 @@ namespace UWPX_UI_Context.Classes.DataTemplates.Controls
     {
         //--------------------------------------------------------Attributes:-----------------------------------------------------------------\\
         #region --Attributes--
+        private string _FreeText;
+        public string FreeText
+        {
+            get => _FreeText;
+            set => SetProperty(ref _FreeText, value);
+        }
 
+        private string _CompanyText;
+        public string CompanyText
+        {
+            get => _CompanyText;
+            set => SetProperty(ref _CompanyText, value);
+        }
+
+        private string _HostedText;
+        public string HostedText
+        {
+            get => _HostedText;
+            set => SetProperty(ref _HostedText, value);
+        }
+
+        private string _PasswordResetText;
+        public string PasswordResetText
+        {
+            get => _PasswordResetText;
+            set => SetProperty(ref _PasswordResetText, value);
+        }
+
+        private string _LegalNoticeUrl;
+        public string LegalNoticeUrl
+        {
+            get => _LegalNoticeUrl;
+            set => SetProperty(ref _LegalNoticeUrl, value);
+        }
+
+        private string _OnlinceSiceText;
+        public string OnlinceSiceText
+        {
+            get => _OnlinceSiceText;
+            set => SetProperty(ref _OnlinceSiceText, value);
+        }
+
+        private string _MamStorageTimeText;
+        public string MamStorageTimeText
+        {
+            get => _MamStorageTimeText;
+            set => SetProperty(ref _MamStorageTimeText, value);
+        }
+
+        private string _MaxUploadSizeText;
+        public string MaxUploadSizeText
+        {
+            get => _MaxUploadSizeText;
+            set => SetProperty(ref _MaxUploadSizeText, value);
+        }
+
+        private string _UploadStorageTime;
+        public string UploadStorageTime
+        {
+            get => _UploadStorageTime;
+            set => SetProperty(ref _UploadStorageTime, value);
+        }
 
         #endregion
         //--------------------------------------------------------Constructor:----------------------------------------------------------------\\
