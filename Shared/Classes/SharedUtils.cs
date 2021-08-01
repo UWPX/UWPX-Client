11f210a94a53ba206c21447ee5966e4ee1368407
Fabian Sauter <sauter.fabian@mailbox.org>
2021-08-01 14:07:54 +0200

    Device ID anonymization to prevent tracking between apps and installations

--- Shared/Classes/SharedUtils.cs
+++ Shared/Classes/SharedUtils.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Logging;
@@ -7,6 +8,7 @@ using Windows.ApplicationModel.Core;
 using Windows.Media.Core;
 using Windows.Media.Playback;
 using Windows.Phone.Devices.Notification;
+using Windows.Storage;
 using Windows.System.Profile;
 using Windows.UI.Core;
 
@@ -28,20 +30,57 @@ namespace Shared.Classes
         #region --Set-, Get- Methods--
         /// <summary>
         /// Returns a hex string representing an unique device ID.
+        /// The device ID is a SHA256 hash, hex string of the actual device ID XOR a device nonce to prevent tracking between apps.
         /// </summary>
         public static string GetUniqueDeviceId()
         {
+            byte[] deviceId;
+
             SystemIdentificationInfo systemId = SystemIdentification.GetSystemIdForPublisher();
             if (systemId.Source != SystemIdentificationSource.None)
             {
-                return ByteArrayToHexString(systemId.Id.ToArray());
+                deviceId = systemId.Id.ToArray();
+            }
+            else
+            {
+                // Fall back to generating a unique ID based on the hardware of the system.
+                // This ID will change once the hardware changes.
+                // Based on: https://montemagno.com/unique-device-id-for-mobile-apps/
+                HardwareToken hwToken = HardwareIdentification.GetPackageSpecificToken(null);
+                deviceId = hwToken.Id.ToArray();
+            }
+            byte[] nonce = GetDeviceNonce();
+            // Ensure the device ID is long enough:
+            deviceId = deviceId.Length >= 32 ? XorShorten(deviceId, nonce) : nonce;
+            SHA256 sha = SHA256.Create();
+            deviceId = sha.ComputeHash(deviceId);
+            return ByteArrayToHexString(deviceId);
+        }
+
+        /// <summary>
+        /// Returns a 64 byte long nonce generated on the first call.
+        /// This nonce is stored persistently.
+        /// </summary>
+        /// <returns>64 byte long device nonce.</returns>
+        private static byte[] GetDeviceNonce()
+        {
+            object nonceObj = null;
+            try
+            { nonceObj = ApplicationData.Current.LocalSettings.Values["DEVICE_NONCE"]; }
+            catch { }
+
+            // Return the existing nonce:
+            if (nonceObj is byte[] nonce && nonce.Length == 64)
+            {
+                return nonce;
             }
 
-            // Fall back to generating a unique ID based on the hardware of the system.
-            // This ID will change once the hardware changes.
-            // Based on: https://montemagno.com/unique-device-id-for-mobile-apps/
-            HardwareToken hwToken = HardwareIdentification.GetPackageSpecificToken(null);
-            return ByteArrayToHexString(hwToken.Id.ToArray());
+            // Create a new nonce:
+            Random r = new Random((int)DateTime.Now.Ticks);
+            nonce = new byte[64];
+            r.NextBytes(nonce);
+            ApplicationData.Current.LocalSettings.Values["DEVICE_NONCE"] = nonce;
+            return nonce;
         }
 
         #endregion
@@ -182,6 +221,29 @@ namespace Shared.Classes
             }
 
             return hex.ToString();
+        }
+
+        /// <summary>
+        /// Calculates the a XOR b.
+        /// The result has the length of the shorter array.
+        /// </summary>
+        /// <param name="a">First input byte array.</param>
+        /// <param name="b">Second input byte array.</param>
+        /// <returns>a XOR b with the length of the shorter array.</returns>
+        private static byte[] XorShorten(byte[] a, byte[] b)
+        {
+            int len = a.Length;
+            if (b.Length < len)
+            {
+                len = b.Length;
+            }
+
+            byte[] result = new byte[len];
+            for (int i = 0; i < len; i++)
+            {
+                result[i] = (byte)(a[i] ^ b[i]);
+            }
+            return result;
         }
 
         #endregion
