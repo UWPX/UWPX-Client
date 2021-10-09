ab5a3eabe9b815d417d031a8e36cbf246224adb4
Fabian Sauter <sauter.fabian@mailbox.org>
2021-10-09 19:59:00 +0200

    Text quotation

--- UWPX_UI/Extensions/TextBlockChatMessageFormatExtension.cs
+++ UWPX_UI/Extensions/TextBlockChatMessageFormatExtension.cs
@@ -6,6 +6,8 @@ using NeoSmart.Unicode;
 using Storage.Classes;
 using UWPX_UI_Context.Classes;
 using Windows.ApplicationModel.Contacts;
+using Windows.UI;
+using Windows.UI.Text;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Documents;
@@ -28,10 +30,14 @@ namespace UWPX_UI.Extensions
         private const string URL_REGEX_PATTERN = @"(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'"".,<>?«»“”‘’]))";
         private const string EMAIL_REGEX_PATTERN = @"(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))";
         private const string PHONE_REGEX_PATTERN = @"\+?(\d{2,}[\-\(\)\. ]?){2,}\d\b";
+        private const string NEW_LINE_REGEX_PATTERN = @"\r\n|\r|\n";
+        private const string QUOTATION_REGEX_PATTERN = @"^\s*>\s*";
 
         private static readonly Regex URL_REGEX = new Regex(URL_REGEX_PATTERN, RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(500));
         private static readonly Regex EMAIL_REGEX = new Regex(EMAIL_REGEX_PATTERN, RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(500));
         private static readonly Regex PHONE_REGEX = new Regex(PHONE_REGEX_PATTERN, RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+        private static readonly Regex NEW_LINE_REGEX = new Regex(NEW_LINE_REGEX_PATTERN);
+        private static readonly Regex QUOTATION_REGEX = new Regex(QUOTATION_REGEX_PATTERN);
 
         public static readonly DependencyProperty FormattedTextProperty = DependencyProperty.Register("FormattedText", typeof(string), typeof(TextBlockChatMessageFormatExtension), new PropertyMetadata("Loading...", OnFormattedTextChanged));
 
@@ -53,6 +59,17 @@ namespace UWPX_UI.Extensions
             obj.SetValue(FormattedTextProperty, value);
         }
 
+        private static SolidColorBrush GetQuoteForeground()
+        {
+            return ThemeUtils.GetThemeResource<SolidColorBrush>("CaptionTextBrush");
+        }
+
+        private static void ToQuoteRun(Run run)
+        {
+            run.Foreground = GetQuoteForeground();
+            run.FontStyle = FontStyle.Italic;
+        }
+
         #endregion
         //--------------------------------------------------------Misc Methods:---------------------------------------------------------------\\
         #region --Misc Methods (Public)--
@@ -63,16 +80,22 @@ namespace UWPX_UI.Extensions
         #region --Misc Methods (Private)--
         /// <summary>
         /// This method will extract a fragment of the raw text string, create a Run element with the fragment and
-        /// add it to the textblock inlines collection
+        /// add it to the <see cref="TextBlock.Inlines"/> collection.
         /// </summary>
-        /// <param name="textBlock">the textblock where to add the run element</param>
-        /// <param name="rawText">the raw text where the fragment will be extracted</param>
-        /// <param name="startPosition">the start position to extract the fragment</param>
-        /// <param name="endPosition">the end position to extract the fragment</param>
-        private static void CreateRunElement(TextBlock textBlock, string rawText, int startPosition, int endPosition)
+        /// <param name="textBlock">The <see cref="TextBlock"/> where to add the run element.</param>
+        /// <param name="rawText">The raw text where the fragment will be extracted.</param>
+        /// <param name="startPosition">The start position to extract the fragment.</param>
+        /// <param name="endPosition">The end position to extract the fragment.</param>
+        /// <param name="isQuote">True in case we are currently inside a quote.</param>
+        private static void CreateRunElement(TextBlock textBlock, string rawText, int startPosition, int endPosition, bool isQuote)
         {
             string fragment = rawText.Substring(startPosition, endPosition - startPosition);
-            textBlock.Inlines.Add(new Run { Text = fragment });
+            Run run = new Run { Text = fragment };
+            if (isQuote)
+            {
+                ToQuoteRun(run);
+            }
+            textBlock.Inlines.Add(run);
         }
 
         /// <summary>
@@ -82,20 +105,19 @@ namespace UWPX_UI.Extensions
         /// <param name="textBlock">the textblock where to add the hyperlink</param>
         /// <param name="urlMatch">the match for the URL to use to create the hyperlink element</param>
         /// <returns>the newest position on the source string for the parsing</returns>
-        private static int CreateUrlElement(TextBlock textBlock, Match urlMatch)
+        private static int CreateUrlElement(TextBlock textBlock, Match urlMatch, bool isQuote)
         {
+            Run run = new Run { Text = urlMatch.Value };
+            if (isQuote)
+            {
+                ToQuoteRun(run);
+            }
             if (Uri.TryCreate(urlMatch.Value, UriKind.RelativeOrAbsolute, out Uri targetUri) && (targetUri.IsAbsoluteUri || Uri.TryCreate(RELATIVE_URI_DEFAULT_PREFIX + targetUri.OriginalString, UriKind.RelativeOrAbsolute, out targetUri)))
             {
+
                 Hyperlink link = new Hyperlink
                 {
-                    Inlines =
-                    {
-                        new Run
-                        {
-                            Text = urlMatch.Value,
-                            Foreground = (Brush)Application.Current.Resources["SpeechBubbleForegroundBrush"]
-                        }
-                    },
+                    Inlines = { run },
                     NavigateUri = targetUri
                 };
                 textBlock.Inlines.Add(link);
@@ -122,16 +144,16 @@ namespace UWPX_UI.Extensions
         /// <param name="emailMatch">the match for the email to use to create the hyperlink element. Set to null if not available but at least one of emailMatch and phoneMatch must be not null.</param>
         /// <param name="phoneMatch">the match for the phone number to create the hyperlink element. Set to null if not available but at least one of emailMatch and phoneMatch must be not null.</param>
         /// <returns>the newest position on the source string for the parsing</returns>
-        private static int CreateContactElement(TextBlock textBlock, Match emailMatch, Match phoneMatch)
+        private static int CreateContactElement(TextBlock textBlock, Match emailMatch, Match phoneMatch, bool isQuote)
         {
             Match currentMatch = emailMatch ?? phoneMatch;
 
-            Hyperlink link = new Hyperlink();
-            link.Inlines.Add(new Run
+            Run run = new Run { Text = currentMatch.Value };
+            if (isQuote)
             {
-                Text = currentMatch.Value,
-                Foreground = (Brush)Application.Current.Resources["SpeechBubbleForegroundBrush"]
-            });
+                ToQuoteRun(run);
+            }
+            Hyperlink link = new Hyperlink { Inlines = { run } };
             link.Click += (s, a) =>
             {
                 Contact contact = new Contact();
@@ -159,7 +181,7 @@ namespace UWPX_UI.Extensions
         #endregion
         //--------------------------------------------------------Events:---------------------------------------------------------------------\\
         #region --Events--
-        private static async void OnFormattedTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private static void OnFormattedTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (d is not TextBlock textBlock)
             {
@@ -183,7 +205,7 @@ namespace UWPX_UI.Extensions
                 return;
             }
 
-            bool isEmoji = await Task.Run(() => Emoji.IsEmoji(text.TrimEnd(UiUtils.TRIM_CHARS).TrimStart(UiUtils.TRIM_CHARS)));
+            bool isEmoji = Emoji.IsEmoji(text.TrimEnd(UiUtils.TRIM_CHARS).TrimStart(UiUtils.TRIM_CHARS));
             if (isEmoji)
             {
                 textBlock.Inlines.Add(new Run
@@ -194,46 +216,65 @@ namespace UWPX_UI.Extensions
             }
             else
             {
-                int lastPosition = 0;
-                Match[] matches = new Match[3] { Match.Empty, Match.Empty, Match.Empty };
-
-                do
+                // Split into lines:
+                string[] lines = NEW_LINE_REGEX.Split(text);
+                Match quoteMatch;
+                for (int i = 0; i < lines.Length; i++)
                 {
-                    try
+                    string line = lines[i];
+                    quoteMatch = QUOTATION_REGEX.Match(line);
+                    if (quoteMatch.Success)
                     {
-                        matches[0] = URL_REGEX.Match(text, lastPosition);
-                        matches[1] = EMAIL_REGEX.Match(text, lastPosition);
-                        matches[2] = PHONE_REGEX.Match(text, lastPosition);
-                    }
-                    catch (RegexMatchTimeoutException) { }
+                        line = line.Substring(quoteMatch.Length);
 
-                    Match firstMatch = matches.Where(x => !(x is null) && x.Success).OrderBy(x => x.Index)?.FirstOrDefault();
-                    if (firstMatch == matches[0])
-                    {
-                        // the first match is an URL:
-                        CreateRunElement(textBlock, text, lastPosition, firstMatch.Index);
-                        lastPosition = CreateUrlElement(textBlock, firstMatch);
-                    }
-                    else if (firstMatch == matches[1])
-                    {
-                        // the first match is an email:
-                        CreateRunElement(textBlock, text, lastPosition, firstMatch.Index);
-                        lastPosition = CreateContactElement(textBlock, firstMatch, null);
-                    }
-                    else if (firstMatch == matches[2])
-                    {
-                        // the first match is a phone number:
-                        CreateRunElement(textBlock, text, lastPosition, firstMatch.Index);
-                        lastPosition = CreateContactElement(textBlock, null, firstMatch);
-                    }
-                    else
-                    {
-                        // no match, we add the whole text:
-                        textBlock.Inlines.Add(new Run { Text = text.Substring(lastPosition) });
-                        lastPosition = text.Length;
                     }
 
-                } while (lastPosition < text.Length);
+                    if (i < lines.Length - 1)
+                    {
+                        line += '\n';
+                    }
+
+                    int lastPosition = 0;
+                    Match[] matches = new Match[3] { Match.Empty, Match.Empty, Match.Empty };
+
+                    do
+                    {
+                        try
+                        {
+                            matches[0] = URL_REGEX.Match(line, lastPosition);
+                            matches[1] = EMAIL_REGEX.Match(line, lastPosition);
+                            matches[2] = PHONE_REGEX.Match(line, lastPosition);
+                        }
+                        catch (RegexMatchTimeoutException) { }
+
+                        Match firstMatch = matches.Where(x => !(x is null) && x.Success).OrderBy(x => x.Index)?.FirstOrDefault();
+                        if (firstMatch == matches[0])
+                        {
+                            // the first match is an URL:
+                            CreateRunElement(textBlock, line, lastPosition, firstMatch.Index, quoteMatch.Success);
+                            lastPosition = CreateUrlElement(textBlock, firstMatch, quoteMatch.Success);
+                        }
+                        else if (firstMatch == matches[1])
+                        {
+                            // the first match is an email:
+                            CreateRunElement(textBlock, line, lastPosition, firstMatch.Index, quoteMatch.Success);
+                            lastPosition = CreateContactElement(textBlock, firstMatch, null, quoteMatch.Success);
+                        }
+                        else if (firstMatch == matches[2])
+                        {
+                            // the first match is a phone number:
+                            CreateRunElement(textBlock, line, lastPosition, firstMatch.Index, quoteMatch.Success);
+                            lastPosition = CreateContactElement(textBlock, null, firstMatch, quoteMatch.Success);
+                        }
+                        else
+                        {
+                            // no match, we add the whole text:
+                            CreateRunElement(textBlock, line, lastPosition, line.Length, quoteMatch.Success);
+                            lastPosition = line.Length;
+                        }
+
+                    } while (lastPosition < line.Length);
+                }
             }
         }
 
