dcd93c9d8868437fcc009aeeb24d7fee77b55279
Fabian Sauter <sauter.fabian@mailbox.org>
2021-12-31 13:02:47 +0100

    Released v.0.39.0.0

--- Storage/Classes/SettingsConsts.cs
+++ Storage/Classes/SettingsConsts.cs
@@ -4,7 +4,7 @@
     {
         public const string INITIALLY_STARTED = "initially_started";
         public const string HIDE_WELCOME_DIALOG = "hide_welcome_dialog";
-        public const string HIDE_WHATS_NEW_DIALOG = "hide_whats_new_dialog_alpha_38";
+        public const string HIDE_WHATS_NEW_DIALOG = "hide_whats_new_dialog_alpha_39";
 
         public const string APP_REQUESTED_THEME = "app_requested_theme";
 
