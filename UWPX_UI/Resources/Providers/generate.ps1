c30118c636cfb320e5b19dd7b6364232e9e5ddd2
Fabian Sauter <sauter.fabian@mailbox.org>
2022-02-01 18:11:01 +0100

    Updated the xmpp providers script

--- UWPX_UI/Resources/Providers/generate.ps1
+++ UWPX_UI/Resources/Providers/generate.ps1
@@ -1,23 +1,23 @@
 # Clone https://invent.kde.org/melvo/xmpp-providers/-/tree/master
 # Then execute this and use the contents inside the 'output' directory.
 
-if (Test-Path -Path output) {
-    Remove-Item -Recurse -Force output
-}
-
-mkdir output
+git clone https://invent.kde.org/melvo/xmpp-providers.git
+cd xmpp-providers
 
 python.exe .\filter.py -A -s
-Move-Item providers-A.json output/providers-A-simple.json
+Move-Item -Force providers-A.json ../providers-A-simple.json
 python.exe .\filter.py -A
-Move-Item providers-A.json output/providers-A.json
+Move-Item -Force providers-A.json ../providers-A.json
 
 python.exe .\filter.py -B -s
-Move-Item providers-B.json output/providers-B-simple.json
+Move-Item -Force providers-B.json ../providers-B-simple.json
 python.exe .\filter.py -B
-Move-Item providers-B.json output/providers-B.json
+Move-Item -Force providers-B.json ../providers-B.json
 
 python.exe .\filter.py -C -s
-Move-Item providers-C.json output/providers-C-simple.json
+Move-Item -Force providers-C.json ../providers-C-simple.json
 python.exe .\filter.py -C
-Move-Item providers-C.json output/providers-C.json
+Move-Item -Force providers-C.json ../providers-C.json
+
+cd ..
+Remove-Item -Recurse -Force xmpp-providers
